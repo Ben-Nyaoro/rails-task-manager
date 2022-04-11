c481949b95634f21128a77d3b0efd587c93052d3
Benjamin Nyaoro <nyaorobenjamin@gmail.com>
2022-04-11 12:36:34 +0200

    Simple form implemented

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,7 @@
 class TasksController < ApplicationController
+
+  before_action :set_task, only: [:show, :edit, :update, :destroy ]
+
 	def index
 		@tasks = Task.all
 	end
@@ -14,21 +17,21 @@ class TasksController < ApplicationController
 	end 
 
 	def show
-		@task = Task.find(params[:id])
+		# @task = Task.find(params[:id])
 	end
 
 	def edit
-		@task = Task.find(params[:id])
+		# @task = Task.find(params[:id])
 	end
 
 	def update	
-		@task = Task.find(params[:id])
+		# @task = Task.find(params[:id])
 		@task.update(task_params)
 		redirect_to task_path(@task)
 	end
 
 	def destroy
-		@task = Task.find(params[:id])
+		# @task = Task.find(params[:id])
 		@task.destroy
 		redirect_to tasks_path
 	end
@@ -37,5 +40,9 @@ private
 
 	def task_params
 		params.require(:task).permit(:title, :details, :completed)
+	end
+
+	def set_task
+		@task = Task.find(params[:id])
 	end
 end
